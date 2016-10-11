dbed5eb24ab369d1cda62d57929ec570068998af
Yaumil Akhir <yaumilakhir@users.noreply.github.com>
2016-10-11 08:53:15 +0700

    render default value for arguments

--- src/Model/ArgumentModel.php
+++ src/Model/ArgumentModel.php
@@ -44,10 +44,16 @@ class ArgumentModel extends RenderableModel
     public function toLines()
     {
         if ($this->type !== null) {
-            return $this->type . ' $' . $this->name;
+            $return = $this->type . ' $' . $this->name;
         } else {
-            return '$' . $this->name;
+            $return = '$' . $this->name;
         }
+
+        if ($this->default !== null) {
+            $return .= ' = ' . $this->default;
+        }
+
+        return $return;
     }
 
     /**
